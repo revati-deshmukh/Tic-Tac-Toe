c0ca753e2d80f061678120e1a9633e536f7f4942
revati-deshmukh <deshmukh.revat@gmail.com>
2022-02-21 20:18:17 +0800

    Added styles to board

--- src/components/Game.js
+++ src/components/Game.js
@@ -50,7 +50,7 @@ class Game extends React.Component {
                 'Go to game start';
             return (
                 <li key={move}>
-                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                    <button className="move-info" onClick={() => this.jumpTo(move)}>{desc}</button>
                 </li>
             );
         });
